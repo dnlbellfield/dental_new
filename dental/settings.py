edced04acf11f27a491585394b0a5d9d48ca925a
Daniel Bellfield <dnlbellfield@gmail.com>
2021-03-12 00:43:03 -0800

    mess up typo

--- dental/settings.py
+++ dental/settings.py
@@ -1,10 +1,10 @@
 import os
 import django_heroku
-heroku logs --tail
+import dj_database_url
 from decouple import config
 from pathlib import Path
 from vitamin import key, passy
-# import dj-database-url
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
