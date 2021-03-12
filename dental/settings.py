acbda9e8453ed3d9792469ce879a1c76f63b5c42
Daniel Bellfield <dnlbellfield@gmail.com>
2021-03-12 00:46:39 -0800

    typo

--- dental/settings.py
+++ dental/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
 }
 
 db_from_env = dj_database_url.config()
-DATABASES[‘default’].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
