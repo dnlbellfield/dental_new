e86fc283a20e6d2a2f05dceb6f4e16980bd11bb9
Daniel Bellfield <dnlbellfield@gmail.com>
2021-03-12 23:23:12 -0800

    new assests, new_index, new_contact views/urls

--- website/urls.py
+++ website/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.new_index, name='new_index'),
+    # path('', views.home, name='home'),
     path('contact', views.contact, name='contact'),
-    path('new_index/', views.new_index, name='new_index'),
+    
 
     ]
