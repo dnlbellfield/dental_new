26d6e4568af825508781b8d8abdb5ede07c89cb5
Daniel Bellfield <dnlbellfield@gmail.com>
2021-03-17 00:42:21 -0700

    collect static after mailgun

---
+++ staticfiles/website/newJS/new_jsjs.js
@@ -0,0 +1,17 @@
+// var buttonBackground = document.getElementById('theBackButton');
+// console.log('this is the submit button', buttonBackground);
+// 	buttonBackground.addEventListener("mouseover", logEvent);
+
+//   (function(){
+//     console.log(buttonBackground);
+//   }())
+	
+// 	function logEvent(event){
+// 		if (event){
+//       buttonBackground.style.background='#000000';
+// 			console.log('event happened');
+// 		} else {
+// 				console.log('nothing')
+// 		}
+// 	}
+
